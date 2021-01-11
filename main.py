2e9eb49a024c83de29cbb372a2dce97f993dfae2
vmaddara <mv0410@s1vsk.lv>
2021-01-11 12:49:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Komentārus var rakstīt
+vairākās
+rindās"""
+print("ELLO WORLD")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "pitons")
+print(f"Ja saskaitīsim 5 ar 7 iegūsim {5+7}.") #kombinētā izdruka
+print("Sveika, "+"pasaule!")
+print("Madara "*5)
+print(365*24*60*60)
+print(0.1 + 0.2 - 0.3)
+print()
