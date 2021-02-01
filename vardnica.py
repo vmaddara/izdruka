d0a314514ce0901845320a2c527f4586117b259b
vmaddara <mv0410@s1vsk.lv>
2021-02-01 13:21:47 +0000

    vārdnīcas-pievieno jaunus objektus

--- vardnica.py
+++ vardnica.py
@@ -1,4 +1,5 @@
 #vārdnīcas jeb dictionaries
+#atslēga:vērtība
 tel = {
     "direktors": "27300486",
     "vietnieks": "25647731",
@@ -12,4 +13,24 @@ cenas = {"piens": 1.12, "āboli": 0.95, "apelsīni": 1.89}
 print(cenas["piens"])
 print(cenas["apelsīni"])
 
-dict_ = {"k1": 123, "k2": [10, 11, 12], "k3": {"atsl1": 100, "atsl2": 200}}
+d = {"k1": 123, "k2": [10, 11, 12], "k3": {"atsl1": 100, "atsl2": 200}}
+#vārdnīcās var uzglabāt dažādus datu tipus
+print(d["k3"]["atsl1"])  #izdrukā iekšējās vārdnīcas vērtību
+print(d["k2"][2])  #izdrukā saraksta elementu
+
+myDict = {"key1": ['a', 'b', 'c']}
+print(myDict)
+myList = myDict['key1']
+print(myList)
+burts= myList[2]
+print(burts)
+print(burts.upper())#izdrukā lielo C,kas atrodas vārdnīcas vērtībā
+print(myDict["key1"][2].upper()) #vienā rindā atlasa elementu un pārveido
+
+#pievieno jaunus objektus
+newDict={"k1":100, "k2": 200}
+print(newDict)
+newDict["k3"]=300
+print(newDict)
+newDict["k1"]="simts"
+print(newDict)
